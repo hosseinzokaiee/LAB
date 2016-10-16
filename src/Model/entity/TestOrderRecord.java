a6b6661328b79615ede3d97ada5bb0dffee2fb29
hosseinzokaiee <hossein.zokaiee@yahoo.com>
2016-10-16 11:20:44 +0330

    Entity

---
+++ src/Model/entity/TestOrderRecord.java
@@ -0,0 +1,32 @@
+package Model.entity;
+
+import java.security.SecureRandom;
+import java.util.Date;
+
+/**
+ * Created by Hossein on 16/10/2016.
+ */
+public class TestOrderRecord implements SecureRandom {
+    private int Id;
+    private String ActionCode;
+    private byte ApplicationCode;
+    private String Cobastype;
+    private String Containertype;
+    private Date CreateDate;
+    private byte Dilution;
+    private int MessageId;
+    private String PositionNo;
+    private String Priority;
+    private String RackID;
+    private String Reporttypes;
+    private String ResultDate;
+    private String SampleType;
+    private String SequenceNo;
+    private String Sequencenumber;
+    private String SpecimenCollectionDate;
+    private String SpecimenDiscriptor;
+    private String SpecimenID;
+    private String TypeId;
+
+
+}
